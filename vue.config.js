898803c9b9d2f2726dedc8d83ca96e4ce231e050
mandy1219 <729147826@qq.com>
2021-12-02 16:24:22 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
           lessOptions: {
             modifyVars: {
               // 直接覆盖变量(这里不需要@符号)
-              // 'green': '#111',
+              'blue': '#477CFF',
               // 'border-color': '#eee',
               // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
               hack: `true`,
